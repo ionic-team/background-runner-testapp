65f9adfe0a2400b045ed8e51a13395f546ce2401
Joseph Pender <joey@ionic.io>
2024-04-08 10:50:10 -0500

    fmt

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,22 +1,22 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.capacitorjs.background.testapp",
-  appName: "background-runner-testapp",
-  webDir: "dist",
-  server: {
-    androidScheme: "https",
-  },
-  plugins: {
-    BackgroundRunner: {
-      label: "com.capacitorjs.background.testapp.task",
-      src: "assets/background.js",
-      event: "updateData",
-      repeat: true,
-      interval: 30,
-      autoStart: false,
+    appId: "com.capacitorjs.background.testapp",
+    appName: "background-runner-testapp",
+    webDir: "dist",
+    server: {
+        androidScheme: "https",
     },
-  },
+    plugins: {
+        BackgroundRunner: {
+            label: "com.capacitorjs.background.testapp.task",
+            src: "assets/background.js",
+            event: "updateData",
+            repeat: true,
+            interval: 15,
+            autoStart: true,
+        },
+    },
 };
 
 export default config;
