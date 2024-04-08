7679bf9e2802c7fab71fc64015731ac7c8eda520
Joseph Pender <joey@ionic.io>
2024-04-08 10:48:02 -0500

    log attempts ad updating weather and news

--- src/background.js
+++ src/background.js
@@ -1,10 +1,13 @@
 async function checkWeather() {
+  console.log("checking weather...");
+
   let location;
+
   try {
-    console.log("checking weather...");
     location = await CapacitorGeolocation.getCurrentPosition();
   } catch (err) {
     console.error(`Could not get current location: ${err}`);
+    throw err;
   }
 
   try {
@@ -24,31 +27,183 @@ async function checkWeather() {
       conditionIcon: weather.weather[0].icon ?? "",
     };
 
-    CapacitorKV.set(
-      "CapacitorStorage.cached_weather",
-      JSON.stringify(conditions)
-    );
+    CapacitorKV.set("cached_weather", JSON.stringify(conditions));
   } catch (err) {
     console.error(`Could not check weather: ${err}`);
+    throw err;
   }
 }
 
 async function checkNews() {
   console.log("checking news...");
+
+  try {
+    const appleDeveloperNewsFeed =
+      "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fdeveloper.apple.com%2Fnews%2Frss%2Fnews.rss";
+    const response = await fetch(appleDeveloperNewsFeed);
+
+    if (!response.ok) {
+      throw new Error("Fetch GET request failed");
+    }
+
+    const feed = await response.json();
+
+    let stories = {};
+
+    if (feed.status != "ok") {
+      throw new Error("could not download rss to json feed");
+    }
+
+    feed.items.map((item) => {
+      const textDescription = item.description.replace(/<[^>]*>?/gm, "");
+      let teaser = textDescription.substring(0, 25) + "...";
+      stories[item.guid] = {
+        title: item.title,
+        teaser,
+        link: item.link,
+        publishDate: undefined,
+      };
+    });
+
+    let newStories = [];
+    let existingStories = {};
+
+    const result = CapacitorKV.get("cached_stories");
+
+    if (result) {
+      existingStories = JSON.parse(result.value);
+    }
+
+    Object.keys(stories).forEach((key) => {
+      if (!existingStories[key]) {
+        newStories.push(stories[key]);
+        existingStories[key] = stories[key];
+      }
+    });
+
+    CapacitorKV.set("cached_stories", JSON.stringify(existingStories));
+
+    console.log(`${newStories.length} new stories`);
+
+    if (newStories.length > 0) {
+      const latestStory = newStories[0];
+
+      let scheduleDate = new Date();
+      scheduleDate.setSeconds(scheduleDate.getSeconds() + 20);
+
+      CapacitorNotifications.schedule([
+        {
+          id: 100,
+          title: latestStory.title,
+          body: latestStory.teaser,
+          scheduleAt: scheduleDate,
+        },
+      ]);
+
+      console.log("news story notification scheduled");
+    }
+  } catch (err) {
+    console.error(`Could not get news stories: ${err}`);
+    throw err;
+  }
 }
+
+addEventListener("testThrowingError", async (resolve, reject, args) => {
+  try {
+    throw new Error("some simulated error");
+  } catch (err) {
+    reject(err);
+  }
+});
 
 addEventListener("updateData", async (resolve, reject, args) => {
   try {
-    await Promise.all([checkWeather(), checkNews()]);
+    const results = await Promise.allSettled([checkWeather(), checkNews()]);
+
     const currentTimestamp = Math.floor(Date.now() / 1000);
 
-    CapacitorKV.set(
-      "CapacitorStorage.last_updated",
-      currentTimestamp.toString()
-    );
+    let updateLog;
+
+    let updateLogJSON = CapacitorKV.get("update_log");
+
+    if (!updateLogJSON) {
+      updateLog = {
+        news: [],
+        weather: [],
+      };
+    } else {
+      updateLog = JSON.parse(updateLogJSON.value);
+    }
+
+    if (results[0].status == "fulfilled") {
+      updateLog.weather.push({
+        timestamp: currentTimestamp,
+        status: "ok",
+      });
+    } else {
+      updateLog.weather.push({
+        timestamp: currentTimestamp,
+        status: `failed: ${results[0].reason}`,
+      });
+    }
+
+    if (results[1].status == "fulfilled") {
+      updateLog.news.push({
+        timestamp: currentTimestamp,
+        status: "ok",
+      });
+    } else {
+      updateLog.news.push({
+        timestamp: currentTimestamp,
+        status: `failed: ${results[1].reason}`,
+      });
+    }
+
+    CapacitorKV.set("last_updated", currentTimestamp.toString());
+    CapacitorKV.set("update_log", JSON.stringify(updateLog));
     resolve();
   } catch (err) {
     console.error(`Could not update data: ${err}`);
+    reject(err);
+  }
+});
+
+addEventListener("getUpdateLog", (resolve, reject, args) => {
+  try {
+    const result = CapacitorKV.get("update_log");
+    resolve(result);
+  } catch (err) {
+    console.error(`Could not get update log: ${err}`);
+    reject(err);
+  }
+});
+
+addEventListener("getStories", (resolve, reject, args) => {
+  try {
+    const result = CapacitorKV.get("cached_stories");
+    resolve(result);
+  } catch (err) {
+    console.error(`Could not get cached news stories: ${err}`);
+    reject(err);
+  }
+});
+
+addEventListener("getCurrentConditions", (resolve, reject, args) => {
+  try {
+    const result = CapacitorKV.get("cached_weather");
+    resolve(result);
+  } catch (err) {
+    console.error(`Could not get current weather conditions: ${err}`);
+    reject(err);
+  }
+});
+
+addEventListener("getLastUpdated", (resolve, reject, args) => {
+  try {
+    const result = CapacitorKV.get("last_updated");
+    resolve(result);
+  } catch (err) {
+    console.error(`Could not get last updated timestamp: ${err}`);
     reject(err);
   }
 });
--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,7 +1,16 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { App } from "@capacitor/app";
-import { Preferences } from "@capacitor/preferences";
+import { BackgroundRunner } from "@capacitor/background-runner";
 
+export interface UpdateLogEntry {
+  timestamp: number;
+  status: string;
+}
+
+export interface UpdateLog {
+  news: UpdateLogEntry[];
+  weather: UpdateLogEntry[];
+}
 export interface WeatherCondition {
   location: string;
   temp: number;
@@ -16,17 +25,28 @@ export interface NewsStory {
   publishDate?: Date;
 }
 
+interface NewsStories {
+  [key: string]: NewsStory;
+}
+
 interface AppContextProps {
   conditions: WeatherCondition;
-  stories: NewsStory[];
+  stories: NewsStories;
+  log: UpdateLog;
   lastUpdated?: Date;
 
   update: () => Promise<any>;
+  getLog: () => Promise<void>;
 }
 
 interface AppProviderProps {
   children?: React.ReactNode;
 }
+
+const emptyUpdateLog: UpdateLog = {
+  news: [],
+  weather: [],
+};
 
 const defaultCondition: WeatherCondition = {
   location: "---",
@@ -37,22 +57,30 @@ const defaultCondition: WeatherCondition = {
 
 export const AppContext = createContext<AppContextProps>({
   conditions: defaultCondition,
-  stories: [],
+  stories: {},
+  log: emptyUpdateLog,
   lastUpdated: undefined,
-  update: () => Promise.resolve(),
+  update: () => Promise.reject(),
+  getLog: () => Promise.reject(),
 });
 
 export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
   const [conditions, setConditions] =
     useState<WeatherCondition>(defaultCondition);
-  const [stories, setStories] = useState<NewsStory[]>([]);
+  const [stories, setStories] = useState<NewsStories>({});
+  const [log, setLog] = useState<UpdateLog>(emptyUpdateLog);
   const [lastUpdated, setLastUpdated] = useState<Date | undefined>(undefined);
 
   const updateStories = async (): Promise<void> => {
     try {
-      const result = await Preferences.get({ key: "cached_stories" });
-      if (result.value) {
-        const cachedStories = JSON.parse(result.value) as NewsStory[];
+      const result = await BackgroundRunner.dispatchEvent<{ value: string }>({
+        event: "getStories",
+        label: "com.capacitorjs.background.testapp.task",
+        details: {},
+      });
+
+      if (result && result.value) {
+        const cachedStories = JSON.parse(result.value) as NewsStories;
         setStories(cachedStories);
       } else {
         console.warn("No value for key 'cached_stories'");
@@ -64,8 +92,15 @@ export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
 
   const updateWeatherConditions = async (): Promise<void> => {
     try {
-      const result = await Preferences.get({ key: "cached_weather" });
-      if (result.value) {
+      const result = await BackgroundRunner.dispatchEvent<{ value: string }>({
+        event: "getCurrentConditions",
+        label: "com.capacitorjs.background.testapp.task",
+        details: {},
+      });
+
+      console.log(JSON.stringify(result));
+
+      if (result && result.value) {
         const cachedWeather = JSON.parse(result.value) as WeatherCondition;
         setConditions(cachedWeather);
       } else {
@@ -77,9 +112,19 @@ export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
   };
 
   const update = async (): Promise<any> => {
-    await Promise.all([updateStories(), updateWeatherConditions()]);
-    const result = await Preferences.get({ key: "last_updated" });
-    if (result.value) {
+    await Promise.all([updateWeatherConditions(), updateStories()]);
+
+    await getLog();
+
+    const result = await BackgroundRunner.dispatchEvent<{ value: string }>({
+      event: "getLastUpdated",
+      label: "com.capacitorjs.background.testapp.task",
+      details: {
+        currentDate: new Date(),
+      },
+    });
+
+    if (result && result.value) {
       const timestamp = parseInt(result.value);
       const timestampDate = new Date(timestamp * 1000);
       setLastUpdated(timestampDate);
@@ -88,14 +133,33 @@ export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
     }
   };
 
+  const getLog = async (): Promise<void> => {
+    const result = await BackgroundRunner.dispatchEvent<{ value: string }>({
+      event: "getUpdateLog",
+      label: "com.capacitorjs.background.testapp.task",
+      details: {},
+    });
+
+    if (result && result.value) {
+      const latestLog = JSON.parse(result.value) as UpdateLog;
+      setLog(latestLog);
+    } else {
+      console.warn("no value for key `update_log`");
+    }
+  };
+
   useEffect(() => {
     update();
 
-    App.addListener("appStateChange", ({ isActive }) => {
-      if (isActive) {
+    const handle = App.addListener("appStateChange", (appState) => {
+      if (appState.isActive) {
         update();
       }
     });
+
+    return () => {
+      handle.remove();
+    };
   }, []);
 
   return (
@@ -103,8 +167,10 @@ export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
       value={{
         conditions,
         stories,
+        log,
         lastUpdated,
         update,
+        getLog,
       }}
     >
       {children}
